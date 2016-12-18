411a099d775d116bd36a5ea3915d388087ff2725
Dasun Karunathilaka <dinindudasun.14@cse.mrt.ac.lk>
2016-12-18 20:05:14 +0530

    Delete function does not work yet

--- Administrator/userlist.php
+++ Administrator/userlist.php
@@ -1,14 +1,14 @@
 <?php require_once('inc/connection.php'); ?>
 <?php 
 
-	$query = "SELECT NIC, name, email, address, occupation FROM customer";
+	$query = "SELECT NIC, name, email, address, occupation, role FROM customer JOIN login ON customer.username = login.username";
 
 	$result_set = mysqli_query($connection, $query);
 
 	if ($result_set){
 
 		$table = '<table>';
-		$table .= '<tr><th>NIC Number</th><th>Customer Name</th><th>Email address</th><th>Address</th><th>Occupation</th></tr>';
+		$table .= '<tr><th>NIC Number</th><th>Customer Name</th><th>Email address</th><th>Address</th><th>Occupation</th><th>System Role</th></tr>';
 
 		while ($record = mysqli_fetch_assoc($result_set)){
 			$table .= '<tr>';
@@ -17,6 +17,8 @@
 			$table .= '<td>' . $record['email'] . '</td>';
 			$table .= '<td>' . $record['address'] . '</td>';
 			$table .= '<td>' . $record['occupation'] . '</td>';
+			$table .= '<td>' . $record['role'] . '</td>';
+			$table .= '<td><input name="delete" type="submit" value="Remove" onclick="return confirm(\'Remove this user?\')"></td>';
 			$table .= '</tr>';
 		}
 	}
@@ -26,8 +28,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+	<link href = "../style/css/hoverbutton.css" rel = "stylesheet">
+
+
 	<meta charset="UTF-8">
-	<title>Busses</title>
+	<title>Users</title>
 	<style>
 		table {border-collapse: collapse;} 
 		td, th {border: 1px solid black; padding: 10px;}
@@ -35,10 +41,11 @@
 </head>
 <body>
 	<header>
-		<h1>Bus List</h1>
+		<h1>Users</h1>
 	</header>
 
 	<?php echo "$table"; ?>
+	
 </body>
 </html>
 
