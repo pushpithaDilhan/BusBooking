357a4d26c93d3f8c9c711c3dd9a290b176765580
shankajarajapakse <sankaja.14@cse.mrt.ac.lk>
2016-12-18 01:05:46 -0800

    Bootstrap added!

---
+++ Operator/booking.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+
+<head><h1>Insert form booking db</h1></head>
+<body>
+<form action="validatebooking.php" method="post">
+Customer Id: <input type="text" name="customer_id"><br><br>
+Bus registration number: <input type="text" name="bus_id"><br><br>
+Seats to be booked: <input type="text" name="seat_count"><br><br>
+Start: <input type="text" name="start"><br><br>
+End: <input type="text" name="ending"><br><br>
+<input type="submit" value="Reserve">
+</form>
+
+</body>
+
+</html>
+
+
