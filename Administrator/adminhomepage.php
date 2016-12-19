5f080d6d49e83d2f1da20c386eb1c926887afa9c
Dasun Karunathilaka <dinindudasun.14@cse.mrt.ac.lk>
2016-12-19 19:19:40 +0530

    All functions work

--- Administrator/adminhomepage.php
+++ Administrator/adminhomepage.php
@@ -3,8 +3,53 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Admin-Home</title>
+	<link href = "../style/css/bootstrap.min.css" rel = "stylesheet">
+	<link rel="stylesheet" href="styles.css">
+
 </head>
-<body>
+<body style="background-color:#3399ff;">
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="../common/routelist.php">Search Routes</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+
+                <li>
+                    <a href="../Administrator/adminhomepage.php" >Admin test</a>
+                </li>
+
+                <li>
+                    <a href="../Register/add-record-form.php" style="text-align:right;position:fixed;">SignUp</a>
+                </li>
+
+
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
 	<header>
 		<h1>Welcome Admin</h1>
 	</header>
@@ -16,9 +61,9 @@
 
 	<input type="submit" value="View Busses" name="view_busses" onclick="parent.location=('busses.php')">
 	<input type="submit" value="View Routes" name="view_routes" onclick="parent.location=('busroutes.php')">
-	<input type="submit" value="View Users" name="view_users" onclick= "parent.location=('userlist.php')">
-
-
+	<input type="submit" value="View Customers" name="view_customers" onclick= "parent.location=('userlist.php')">
+	<input type="submit" value="View Bus Owners" name="view_owners" onclick= "parent.location=('owner_list.php')">
+	<input type="submit" value="View System Administrators" name="view_admins" onclick= "parent.location=('admin_list.php')">
 
 </body>
 </html>
--- Administrator/owner_list.php
+++ Administrator/owner_list.php
@@ -1,14 +1,14 @@
 <?php require_once('inc/connection.php'); ?>
 <?php 
 
-	$query = "SELECT NIC, name, email, address, occupation FROM customer";
+	$query = "SELECT NIC, name, email, address FROM owner";
 
 	$result_set = mysqli_query($connection, $query);
 
 	if ($result_set){
 
 		$table = '<table>';
-		$table .= '<tr><th>NIC Number</th><th>Customer Name</th><th>Email address</th><th>Address</th><th>Occupation</th></tr>';
+		$table .= '<tr><th>NIC Number</th><th>Bus Owner Name</th><th>Email address</th><th>Address</th></tr>';
 
 		while ($record = mysqli_fetch_assoc($result_set)){
 			$table .= '<tr>';
@@ -16,8 +16,8 @@
 			$table .= '<td>' . $record['name'] . '</td>';
 			$table .= '<td>' . $record['email'] . '</td>';
 			$table .= '<td>' . $record['address'] . '</td>';
-			$table .= '<td>' . $record['occupation'] . '</td>';
-			$table .= "<td><a onClick=\"javascript: return confirm('Please confirm deletion');\" href='deleteuser.php?id=".$record['NIC']."'>Remove</a></td><tr>";
+			
+			$table .= "<td><a onClick=\"javascript: return confirm('Please confirm deletion');\" href='delete_owner.php?id=".$record['NIC']."'>Remove</a></td><tr>";
 			$table .= '</tr>';
 		}
 	}
@@ -28,7 +28,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Customers</title>
+	<title>Bus Owners</title>
 	<style>
 		table {border-collapse: collapse;} 
 		td, th {border: 1px solid black; padding: 10px;}
@@ -36,7 +36,7 @@
 </head>
 <body>
 	<header>
-		<h1>Customers</h1>
+		<h1>Bus Owners</h1>
 	</header>
 
 	<?php echo "$table"; ?>
